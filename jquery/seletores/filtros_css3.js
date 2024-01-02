3da0e1518be4ab78be92d8d0c09d392e694c256c
GeorgeSalu <george.salu10@gmail.com>
2024-01-02 15:53:43 -0300

    jquery - seletores - criando o arquivo filtros_css3.js

---
+++ jquery/seletores/filtros_css3.js
@@ -0,0 +1,17 @@
+// selecionando o ultimo elemento, mesmo que seja mas de um caso
+//$('.cars_list li:last-child').css('text-decoration', 'underline');
+
+// selecionando o primeiro elemento, mesmo que seja mas de um caso
+// $('.cars_list li:first-child').css('font-size', '25px')
+
+//selecionando os elementos de maneira alternada (par)
+//$('.cars_list li:nth-child(even)').css('background-color', '#ccc')
+
+//selecionando os elementos de maneira alternada (impar)
+//$('.cars_list li:nth-child(odd)').css('background-color', '#ccc')
+
+//selecionando um elemento em uma posicao especifica, mesmo que seja mas de um caso
+//$('.cars_list li:nth-child(1)').css('font-size', '40px')
+
+//selecionando um elemento em uma posicao especifica, comecando a contar do ultimo elemento
+//$('.cars_list li:nth-last-child(3)').css('font-wwight','bold')
