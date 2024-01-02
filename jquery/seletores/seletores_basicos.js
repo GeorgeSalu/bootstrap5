b48d26dc8b9533e02493b2587bd6389ca791a60e
GeorgeSalu <george.salu10@gmail.com>
2024-01-02 11:35:41 -0300

    jquery - seletores - criando arquivos seletores_basicos.js

---
+++ jquery/seletores/seletores_basicos.js
@@ -0,0 +1,14 @@
+// nome de tag -> $('h1').css('color', 'red');
+// id $('#logo').html('negocios')
+
+// selecionando pela classe
+//$('.paragrafo').animate({
+//    fontSize: '50px',
+//    opacity: 0.7
+//}, 5000)
+
+// seletor composto
+// $('header nav.menu').css('border', 'solid 1px orange')
+
+// multiplos seletores
+// $('form, section').css('backgroud-color','#ccc')
