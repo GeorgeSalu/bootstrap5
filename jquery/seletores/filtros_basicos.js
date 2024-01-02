052175ef1a5941f2e3a79c6b59c2077eb3c59163
GeorgeSalu <george.salu10@gmail.com>
2024-01-02 15:09:28 -0300

    jquery - seletores - separando o conteudo de filtros basicos em filtros_basicos.js

---
+++ jquery/seletores/filtros_basicos.js
@@ -0,0 +1,30 @@
+/*
+selecionando o ultimo elemento
+
+$('.cars_list li:last').click(function() {
+    alert('ok')
+})
+*/
+
+
+/*
+selecionando o primeiro elemento
+
+$('.cars_list li:first').click(function() {
+    alert('ok')
+})
+*/
+
+// selecionando elementos de maneira alternada, comecando pelos impares
+//$('.cars_list li:even').css('background-color', '#ccc');
+
+// selecionando elementos de maneira alternada, comecando pelos pares
+//$('.cars_list li:odd').css('background-color', '#ccc');
+
+// selecionando um elemento em uma posicao especifica (baseado em um array)
+// $('.cars_list li:eq(0)').css('font-size', '40px')
+
+// selecionando todos os elementos abaixo de outro
+// $('.cars_list li:gt(2)').css('opacity', '0.3')
+
+// selecionando todos os elementos acima de outro
