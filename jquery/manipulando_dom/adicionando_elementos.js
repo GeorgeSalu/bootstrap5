c2253ccffca340b41183a30599f300ac6e55f240
GeorgeSalu <george.salu10@gmail.com>
2024-01-03 14:58:22 -0300

    jquery - manipulando o dom - removendo elementos html

---
+++ jquery/manipulando_dom/adicionando_elementos.js
@@ -0,0 +1,30 @@
+// adicionando um elemento ao final de outro
+// $('.menu ul').append('<li><a href="#">news</a></li>')
+
+// adicionando um elemento ao final de outro com outra syntaxe
+// $('<li><a href="#">news2</a></li>').appendTo('.menu ul')
+
+// adicionando um elemento no inciio de outro
+// $('.menu ul').prepend('<li><a href="#">produtos</a></li>')
+
+// adicionando um elemento no inciio de outro em outra sintaxe
+// $('<li><a href="#">produtos</a></li>').prependTo('.menu ul')
+
+// adicionando um elemento apos outro
+// $('.paragrafo').after('<h2>Hcode treinamentos</h2>');
+
+//  adicionando um elemento apos outro, com uma outra sintaxe
+// $('<h2>Hcode treinamentos</h2>').insertAfter('.paragrafo')
+
+// adicionando um elemento antes de outro
+// $('.paragrafo').before('<h2>introducao</h2>')
+
+
+// adicionando um elemento antes de outro com outra sintaxe
+// $('<h2>introducao</h2>').insertBefore('.paragrafo')
+
+// adicionando um elemento em volta de outro
+// $('.cars_list li').wrap('<i></i>')
+
+// adicionando um elemento em volta do conteudo de outro
+// $('.cars_list li').wrapInner('<i></i>')
