992219669e25dc8ec45f01c9cf7da4c8251438d5
GeorgeSalu <george.salu10@gmail.com>
2024-01-02 23:08:53 -0300

    jquery - manipulando o dom - manipulando as dimensoes dos elementos criando arquivos manipulando_dimensoes.js

---
+++ jquery/manipulando_dom/manipulando_dimensoes.js
@@ -0,0 +1,11 @@
+// retorna ou define a largura de um elemento
+// $('.paragrafo').width(500)
+
+// retorna ou define a largura de um elemento, levando em conta o padding
+// $('.paragrafo').innerWidth();
+
+// retorna ou define a altura de um elemento
+// $('.paragrafo').height(100);
+
+// retorna ou define a altura de um elemento, levando em conta o padding
+// $('.paragrafo').innerHeight();
