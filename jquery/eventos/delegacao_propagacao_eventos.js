0dad7b9b93d7da189bf174572708f39c759ae225
GeorgeSalu <george.salu10@gmail.com>
2024-01-04 15:37:29 -0300

    jquery - eventos jquery - retirando um evento de um elemento

--- jquery/eventos/delegacao_propagacao_eventos.js
+++ jquery/eventos/delegacao_propagacao_eventos.js
@@ -19,3 +19,18 @@ $('#btn_add_book').on('click', function() {
   $('#books_list tbody').append(tr);
 
 })
+
+// parando a propagacao de eventos
+$('#form_login').on('change', '[name]', function() {
+
+  alert('elemento mudou, seu valor : '+$(this).val())
+
+})
+
+$('#form_login [name=bio]').on('change',function(event) {
+
+  event.stopPropagation();
+
+  $(this).css('background', 'red')
+
+} )
