14609e6f3e4910e73c5963d64a523742a706e0a9
GeorgeSalu <george.salu10@gmail.com>
2024-01-04 10:53:27 -0300

    jquery - eventos jquery - eventos de teclado

---
+++ jquery/eventos/eventos_teclado.js
@@ -0,0 +1,27 @@
+// evento para quando pressionamos um tecla
+/*
+$('#form_login [name=username]').keydown(function(event) {
+
+    console.log(`voce apertou a tecla com o codigo ${event.keyCode}`)
+
+})
+*/
+
+
+// evento para quando soltarmos uma tecla
+/*
+$('#form_login [name=username]').keyup(function(event) {
+
+    console.log(`voce apertou a tecla com o codigo ${event.keyCode}`)
+
+})
+*/
+
+// evento para quando uma tecla estiver sendo pressionada
+/*
+$('#form_login [name=username]').keypress(function(event) {
+
+    console.log(`voce apertou a tecla com o codigo ${event.keyCode}`)
+
+})
+*/
