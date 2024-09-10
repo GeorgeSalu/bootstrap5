50c496dff1e54dc0e1a851f57bfd14f067c4739d
GeorgeSalu <george.salu10@gmail.com>
2024-09-09 21:13:40 -0300

    bootstrap 5 - modulo componentes - tooltip

--- hora-de-codar/9_componentes/2_aulas/js/scripts.js
+++ hora-de-codar/9_componentes/2_aulas/js/scripts.js
@@ -3,3 +3,10 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerL) {
     return new bootstrap.Popover(popoverTriggerL)
 })
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerL) {
+    return new bootstrap.Tooltip(tooltipTriggerL)
+})
+
