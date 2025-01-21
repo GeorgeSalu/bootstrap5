ddd8307fd3eb6d34ee377bf04963889bc8811a56
George Salu <george.salu10@gmail.com>
2025-01-20 21:26:02 -0300

    projeto menu - criacao da metodo que para logica de carregar etapas do carrinho parte 2

--- menu/js/app.js
+++ menu/js/app.js
@@ -162,6 +162,7 @@ cardapio.metodos = {
         if(abrir) {
 
             $('#modalCarrinho').removeClass('hidden');
+            cardapio.metodos.carregarEtapa(1);
         } else {
 
             $('#modalCarrinho').addClass('hidden');
@@ -180,16 +181,47 @@ cardapio.metodos = {
             $("#resumoCarrinho").addClass('hidden')
 
             $(".etapa").removeClass('active');
-            $(".etapa").removeClass('active');
-            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+
+            $("#btnEtapaPedido").removeClass('hidden')
+            $("#btnEtapaEndereco").addClass('hidden')
+            $("#btnEtapaResumo").addClass('hidden')
+            $("#btnVoltar").addClass('hidden')
         }
 
         if ( etapa == 2) {
             
+            $("#lblTituloEtapa").text('Endereço de entrega: ')
+            $("#itensCarrinho").addClass('hidden')
+            $("#localEntrega").removeClass('hidden')
+            $("#resumoCarrinho").addClass('hidden')
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+            $(".etapa2").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden')
+            $("#btnEtapaEndereco").removeClass('hidden')
+            $("#btnEtapaResumo").addClass('hidden')
+            $("#btnVoltar").removeClass('hidden')
         }
 
         if ( etapa == 3) {
             
+            $("#lblTituloEtapa").text('Resumo pedido: ')
+            $("#itensCarrinho").addClass('hidden')
+            $("#localEntrega").addClass('hidden')
+            $("#resumoCarrinho").removeClass('hidden')
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+            $(".etapa2").addClass('active');
+            $(".etapa3").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden')
+            $("#btnEtapaEndereco").addClass('hidden')
+            $("#btnEtapaResumo").removeClass('hidden')
+            $("#btnVoltar").removeClass('hidden')
         }
 
     },
